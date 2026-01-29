3d5acd64e5ad87349f7bbdd350a4cdb394e4b2bb
leojamc <leomccafferty@outlook.com>
2026-01-29 22:46:52 +0000

    Created file pyside6_main.py, added code to initialise a window

---
+++ python-files/pyside6_main.py
@@ -0,0 +1,11 @@
+#CODE BY LEO MCCAFFERTY W24046037
+#pyside6_main.py
+
+from PySide6.QtWidgets import QApplication, QWidget
+
+app = QApplication()
+
+window = QWidget()
+window.show()
+
+app.exec()
