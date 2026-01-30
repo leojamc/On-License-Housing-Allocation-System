db6adc6ee9ffd7226e3047bd2dcbe46def0b23b0
leojamc <leomccafferty@outlook.com>
2026-01-30 04:13:19 +0000

    Preparing main.py for method population

--- python-files/main.py
+++ python-files/main.py
@@ -1,2 +1,9 @@
 #CODE BY LEO MCCAFFERTY W24046037
 #main.py
+
+correct_login = "DRHM45" #Password for logging in
+def check_login(login_attempt):
+    if login_attempt == correct_login:
+        return True
+    else:
+        return False
